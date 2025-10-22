488f809efa98988a7d0438a838169eb83c2fbdac
zhenzhenqin <3378405015@qq.com>
2025-10-22 15:34:32 +0800

    完成了设备添加策略的相关功能，以及引入商品管理以及商品分类的相关代码

--- src/views/manage/vm/index.vue
+++ src/views/manage/vm/index.vue
@@ -2,12 +2,7 @@
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
       <el-form-item label="设备编号" prop="innerCode">
-        <el-input
-          v-model="queryParams.innerCode"
-          placeholder="请输入设备编号"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.innerCode" placeholder="请输入设备编号" clearable @keyup.enter="handleQuery" />
       </el-form-item>
       <!-- <el-form-item label="点位Id" prop="nodeId">
         <el-input
@@ -67,42 +62,19 @@
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['manage:vm:add']"
-        >新增</el-button>
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['manage:vm:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['manage:vm:edit']"
-        >修改</el-button>
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['manage:vm:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['manage:vm:remove']"
-        >删除</el-button>
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['manage:vm:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['manage:vm:export']"
-        >导出</el-button>
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['manage:vm:export']">导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
@@ -128,33 +100,30 @@
       </el-table-column>
       <el-table-column label="设备状态" align="center" prop="vmStatus">
         <template #default="scope">
-          <dict-tag :options="vm_status" :value="scope.row.vmStatus"/>
+          <dict-tag :options="vm_status" :value="scope.row.vmStatus" />
         </template>
-      </el-table-column> 
+      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
+          <el-button link type="primary" @click="handleUpdatePolicy(scope.row)"
+            v-hasPermi="['manage:vm:edit']">策略</el-button>
           <el-button link type="primary" @click="handleUpdate(scope.row)" v-hasPermi="['manage:vm:edit']">修改</el-button>
           <!-- <el-button link type="primary" @click="handleDelete(scope.row)" v-hasPermi="['manage:vm:remove']">删除</el-button> -->
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
 
-    <!-- 添加或修改设备管理对话框 --> 
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize"
+      @pagination="getList" />
+
+    <!-- 添加或修改设备管理对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
       <el-form ref="vmRef" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="设备编号" prop="innerCode">
           <span>{{ form.innerCode == null ? "设备编号由系统随机生成" : form.innerCode }}</span>
         </el-form-item>
         <el-form-item label="供货时间" v-if="form.innerCode != null">
-           <span>{{ parseTime(form.lastSupplyTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
+          <span>{{ parseTime(form.lastSupplyTime, '{y}-{m}-{d} {h}:{i}:{s}') }}</span>
         </el-form-item>
         <el-form-item label="设备类型" v-if="form.innerCode != null">
           <div v-for="item in vm_typeList" :key="item.id">
@@ -164,33 +133,23 @@
         <el-form-item label="选择型号" prop="vmTypeId" v-if="form.innerCode == null">
           <!-- <el-input v-model="form.vmTypeId" placeholder="请输入设备型号" /> -->
           <el-select v-model="form.vmTypeId" placeholder="请选择设备型号">
-            <el-option
-              v-for="item in vm_typeList"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            />
+            <el-option v-for="item in vm_typeList" :key="item.id" :label="item.name" :value="item.id" />
           </el-select>
-        </el-form-item> 
+        </el-form-item>
         <el-form-item label="设备容量" v-if="form.innerCode != null">
           <div v-for="item in vm_typeList" :key="item.id">
             <span v-if="item.id == form.vmTypeId">{{ item.channelMaxCapacity }}</span>
           </div>
         </el-form-item>
-        <el-form-item label="设备点位 " prop="nodeId" >
+        <el-form-item label="设备点位 " prop="nodeId">
           <!-- <el-input v-model="form.nodeId" placeholder="请输入点位Id" /> -->
-           <el-select v-model="form.nodeId" placeholder="请选择点位">
-            <el-option
-              v-for="item in nodeList"
-              :key="item.id"
-              :label="item.nodeName"
-              :value="item.id"
-            />
-           </el-select>
+          <el-select v-model="form.nodeId" placeholder="请选择点位">
+            <el-option v-for="item in nodeList" :key="item.id" :label="item.nodeName" :value="item.id" />
+          </el-select>
         </el-form-item>
         <el-form-item label="合作商" v-if="form.innerCode != null">
           <div v-for="item in partnerList" :key="item.id">
-            <span v-if="item.id == form.partnerId">{{ item.partnerName }}</span>  
+            <span v-if="item.id == form.partnerId">{{ item.partnerName }}</span>
           </div>
         </el-form-item>
         <el-form-item label="所属区域" v-if="form.innerCode != null">
@@ -211,6 +170,24 @@
         </div>
       </template>
     </el-dialog>
+
+    <!-- 设置策略对话框 -->
+    <el-dialog title="策略管理" v-model="policyOpen" width="500px" append-to-body>
+      <el-form ref="vmRef" :model="form" label-width="80px">
+        <el-form-item label="选择策略" prop="policyId">
+          <el-select v-model="form.policyId" placeholder="请选择策略">
+            <el-option v-for="item in policyList" :key="item.policyId" :label="item.policyName"
+              :value="item.policyId" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button type="primary" @click="submitForm">确 定</el-button>
+          <el-button @click="cancel">取 消</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -219,6 +196,7 @@ import { listVm, getVm, delVm, addVm, updateVm } from "@/api/manage/vm";
 import { listVm_type } from "@/api/manage/vm_type";
 import { listPartner } from "@/api/manage/partner";
 import { listNode } from "@/api/manage/node";
+import { listPolicy } from "@/api/manage/policy";
 import { loadAllParams } from "@/api/page";
 
 const { proxy } = getCurrentInstance();
@@ -274,6 +252,7 @@ function getList() {
 // 取消按钮
 function cancel() {
   open.value = false;
+  policyOpen.value = false;
   reset();
 }
 
@@ -339,6 +318,17 @@ function handleUpdate(row) {
   });
 }
 
+/* 设备策略设置 */
+const policyOpen = ref(false);
+function handleUpdatePolicy(row) {
+  reset();
+  const _id = row.id
+  getVm(_id).then(response => {
+    form.value = response.data;
+    policyOpen.value = true;
+  });
+}
+
 /** 提交按钮 */
 function submitForm() {
   proxy.$refs["vmRef"].validate(valid => {
@@ -347,6 +337,7 @@ function submitForm() {
         updateVm(form.value).then(response => {
           proxy.$modal.msgSuccess("修改成功");
           open.value = false;
+          policyOpen.value = false;
           getList();
         });
       } else {
@@ -363,12 +354,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除设备管理编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除设备管理编号为"' + _ids + '"的数据项？').then(function () {
     return delVm(_ids);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  }).catch(() => { });
 }
 
 /** 导出按钮操作 */
@@ -402,6 +393,15 @@ function getNodeList() {
   });
 }
 
+/* 获取策略列表 */
+const policyList = ref([]);
+function getPolicyList() {
+  listPolicy(loadAllParams).then(response => {
+    policyList.value = response.rows;
+  });
+}
+
+getPolicyList();
 getNodeList();
 getVm_typeList();
 getPartnerList();
